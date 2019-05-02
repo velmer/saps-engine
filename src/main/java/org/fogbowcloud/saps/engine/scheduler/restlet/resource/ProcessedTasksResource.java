9ee74231689f7d36d1cc2f75afae2c5d2d2ee5f2
Vélmer Oliveira <oliveira.velmer@gmail.com>
2019-05-02 17:22:04 -0300

    Fix indentation

--- src/main/java/org/fogbowcloud/saps/engine/scheduler/restlet/resource/ProcessedTasksResource.java
+++ src/main/java/org/fogbowcloud/saps/engine/scheduler/restlet/resource/ProcessedTasksResource.java
@@ -21,29 +21,29 @@ import org.restlet.resource.ResourceException;
  */
 public class ProcessedTasksResource extends BaseResource {
 
-  private static final Logger LOGGER = Logger.getLogger(ProcessedTasksResource.class);
+	private static final Logger LOGGER = Logger.getLogger(ProcessedTasksResource.class);
 
-  private static final String LOWER_LEFT = "lowerLeft";
+	private static final String LOWER_LEFT = "lowerLeft";
 	private static final String UPPER_RIGHT = "upperRight";
 	private static final String PROCESSING_INIT_DATE = "initialDate";
 	private static final String PROCESSING_FINAL_DATE = "finalDate";
 	private static final String PROCESSING_INPUT_GATHERING_TAG = "inputGatheringTag";
 	private static final String PROCESSING_INPUT_PREPROCESSING_TAG = "inputPreprocessingTag";
 	private static final String PROCESSING_ALGORITHM_EXECUTION_TAG = "algorithmExecutionTag";
-  
-  public ProcessedTasksResource() {
-    super();
-  }
 
-  /**
-   * Returns all processed tasks that matches the execution parameters passed
-   * in {@code representation}.
-   * 
-   * @param representation Entity containing execution parameters.
-   * @return List of processed tasks.
-   */
-  @Post
-  public Representation getProcessedTasksInInterval(Representation representation) {
+	public ProcessedTasksResource() {
+		super();
+	}
+
+	/**
+	 * Returns all processed tasks that matches the execution parameters passed
+	 * in {@code representation}.
+	 *
+	 * @param representation Entity containing execution parameters.
+	 * @return List of processed tasks.
+	 */
+	@Post
+	public Representation getProcessedTasksInInterval(Representation representation) {
 		Form form = new Form(representation);
 
 		String lowerLeftLatitude;
@@ -86,9 +86,9 @@ public class ProcessedTasksResource extends BaseResource {
 				"\tGathering: " + inputGathering + "\n" +
 				"\tPreprocessing: " + inputPreprocessing + "\n" +
 				"\tAlgorithm: " + algorithmExecution + "\n";
-    LOGGER.info(builder);
-    
-    List<ImageTask> tasks = application.searchProcessedTasks(
+		LOGGER.info(builder);
+
+		List<ImageTask> tasks = application.searchProcessedTasks(
 				lowerLeftLatitude,
 				lowerLeftLongitude,
 				upperRightLatitude,
@@ -98,8 +98,8 @@ public class ProcessedTasksResource extends BaseResource {
 				inputPreprocessing,
 				inputGathering,
 				algorithmExecution
-    );
-    
+		);
+
 		JSONArray arr = new JSONArray();
 		for (ImageTask task: tasks) {
 			try {
@@ -107,8 +107,8 @@ public class ProcessedTasksResource extends BaseResource {
 			} catch (JSONException e) {
 				LOGGER.error("Failed to build JSON object of Image Task", e);
 			}
-    }
-    
+		}
+
 		JSONObject resObj = new JSONObject();
 		try {
 			resObj.put("result", arr);
@@ -117,6 +117,6 @@ public class ProcessedTasksResource extends BaseResource {
 		}
 
 		return new StringRepresentation(resObj.toString(), MediaType.APPLICATION_JSON);
-  }
+	}
 
 }
