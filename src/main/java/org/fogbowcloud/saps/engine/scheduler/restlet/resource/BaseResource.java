1c8c4569b1522e06c39eb1034c9d10b65c91c252
Vélmer Oliveira <oliveira.velmer@gmail.com>
2019-05-06 17:23:04 -0300

    Creates a parse from Form to SubmissionParameters

--- src/main/java/org/fogbowcloud/saps/engine/scheduler/restlet/resource/BaseResource.java
+++ src/main/java/org/fogbowcloud/saps/engine/scheduler/restlet/resource/BaseResource.java
@@ -2,9 +2,12 @@ package org.fogbowcloud.saps.engine.scheduler.restlet.resource;
 
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.log4j.Logger;
+import org.fogbowcloud.saps.engine.core.dispatcher.SubmissionParameters;
 import org.fogbowcloud.saps.engine.core.model.SapsUser;
 import org.fogbowcloud.saps.engine.scheduler.restlet.DatabaseApplication;
 import org.restlet.data.Form;
+import org.restlet.data.Status;
+import org.restlet.resource.ResourceException;
 import org.restlet.resource.ServerResource;
 
 import java.text.ParseException;
@@ -14,6 +17,16 @@ import java.util.Date;
 public class BaseResource extends ServerResource {
 
 	private static final Logger LOGGER = Logger.getLogger(BaseResource.class);
+
+	private static final String LOWER_LEFT = "lowerLeft";
+	private static final String UPPER_RIGHT = "upperRight";
+	private static final String PROCESSING_INIT_DATE = "initialDate";
+	private static final String PROCESSING_FINAL_DATE = "finalDate";
+	private static final String PROCESSING_INPUT_GATHERING_TAG = "inputGatheringTag";
+	private static final String PROCESSING_INPUT_PREPROCESSING_TAG = "inputPreprocessingTag";
+	private static final String PROCESSING_ALGORITHM_EXECUTION_TAG = "algorithmExecutionTag";
+	private static final int LATITUDE_INDEX = 0;
+	private static final int LONGITUDE_INDEX = 1;
 
 	protected DatabaseApplication application;
 
@@ -43,6 +56,63 @@ public class BaseResource extends ServerResource {
 		}
 		LOGGER.error("No user with this email or password mismatch.");
 		return false;
+	}
+
+	/**
+	 * Extracts a {@link SubmissionParameters} from specified form.
+	 *
+	 * @param form Form that holds the parameters to be extracted.
+	 * @return Extracted submission parameters.
+	 */
+	SubmissionParameters extractSubmissionParameters(Form form) {
+		String lowerLeftLatitude;
+		String lowerLeftLongitude;
+		String upperRightLatitude;
+		String upperRightLongitude;
+		try {
+			lowerLeftLatitude = extractCoordinate(form, LOWER_LEFT, LATITUDE_INDEX);
+			lowerLeftLongitude = extractCoordinate(form, LOWER_LEFT, LONGITUDE_INDEX);
+			upperRightLatitude = extractCoordinate(form, UPPER_RIGHT, LATITUDE_INDEX);
+			upperRightLongitude = extractCoordinate(form, UPPER_RIGHT, LONGITUDE_INDEX);
+		} catch (Throwable t) {
+			LOGGER.error("Failed to parse coordinates.", t);
+			throw new ResourceException(Status.CLIENT_ERROR_BAD_REQUEST, "All coordinates must be informed.");
+		}
+
+		Date initDate;
+		Date endDate;
+		try {
+			initDate = extractDate(form, PROCESSING_INIT_DATE);
+			endDate = extractDate(form, PROCESSING_FINAL_DATE);
+		} catch (Throwable t) {
+			LOGGER.error("Failed to parse dates.", t);
+			throw new ResourceException(Status.CLIENT_ERROR_BAD_REQUEST, "All dates must be informed.");
+		}
+
+		String inputGathering = form.getFirstValue(PROCESSING_INPUT_GATHERING_TAG);
+		if (inputGathering.isEmpty()) {
+			throw new ResourceException(Status.CLIENT_ERROR_BAD_REQUEST, "Input Gathering must be informed.");
+		}
+		String inputPreprocessing = form.getFirstValue(PROCESSING_INPUT_PREPROCESSING_TAG);
+		if (inputPreprocessing.isEmpty()) {
+			throw new ResourceException(Status.CLIENT_ERROR_BAD_REQUEST, "Input Preprocessing must be informed.");
+		}
+		String algorithmExecution = form.getFirstValue(PROCESSING_ALGORITHM_EXECUTION_TAG);
+		if (algorithmExecution.isEmpty()) {
+			throw new ResourceException(Status.CLIENT_ERROR_BAD_REQUEST, "Algorithm Execution must be informed.");
+		}
+
+		return new SubmissionParameters(
+				lowerLeftLatitude,
+				lowerLeftLongitude,
+				upperRightLatitude,
+				upperRightLongitude,
+				initDate,
+				endDate,
+				inputPreprocessing,
+				inputGathering,
+				algorithmExecution
+		);
 	}
 
 	String extractCoordinate(Form form, String name, int index) {
