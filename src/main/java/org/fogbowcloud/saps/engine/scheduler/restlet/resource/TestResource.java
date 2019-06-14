457d9405cb55ffc953bb3dd7f0cda1d049d654d7
Vélmer Oliveira <oliveira.velmer@gmail.com>
2019-06-14 19:55:45 -0300

    Modifies query reading

--- src/main/java/org/fogbowcloud/saps/engine/scheduler/restlet/resource/TestResource.java
+++ src/main/java/org/fogbowcloud/saps/engine/scheduler/restlet/resource/TestResource.java
@@ -1,8 +1,6 @@
 package org.fogbowcloud.saps.engine.scheduler.restlet.resource;
 
-import org.fogbowcloud.saps.engine.core.model.ImageTask;
 import org.restlet.data.Form;
-import org.restlet.data.Header;
 import org.restlet.data.MediaType;
 import org.restlet.representation.Representation;
 import org.restlet.representation.StringRepresentation;
@@ -10,24 +8,22 @@ import org.restlet.resource.ClientResource;
 import org.restlet.resource.Get;
 import org.restlet.resource.Post;
 import org.restlet.resource.ServerResource;
-import org.restlet.util.Series;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 public class TestResource extends ServerResource {
 
     private static final String REQUEST_ATTR_PROCESSED_IMAGES = "images_id[]";
-    public static final String RESTLET_HTTP_HEADERS = "org.restlet.http.headers";
 
     @SuppressWarnings("unchecked")
     @Post
     public Representation testPost(Representation representation) {
         Form form = new Form(representation);
         ClientResource clientResource = new ClientResource("http://localhost:5000/test");
-//        clientResource.setAttribute("images_id[]", "X");
         clientResource.addQueryParameter("images_id[]", "X");
-//        clientResource.addQueryParameter("images_id[]", "Y");
-//        clientResource.addQueryParameter("images_id[]", "Z");
+        clientResource.addQueryParameter("images_id[]", "Y");
+        clientResource.addQueryParameter("images_id[]", "Z");
         clientResource.get(MediaType.APPLICATION_JSON);
         return new StringRepresentation("TestResource.POST", MediaType.TEXT_PLAIN);
     }
@@ -35,15 +31,11 @@ public class TestResource extends ServerResource {
     @SuppressWarnings("unchecked")
     @Get
     public Representation testGet() {
-        Series<Header> series = (Series<Header>) getRequestAttributes()
-                .get("org.restlet.http.headers");
-
-        String[] imageTasksIdsHeader = series.getValuesArray(REQUEST_ATTR_PROCESSED_IMAGES, true);
-        System.out.println("==> Arrays.toString(imageTasksIdsHeader): " + Arrays.toString(imageTasksIdsHeader));
-
-        Form query = getQuery();
-        String[] imageTasksIdsQuery = query.getValues("images_id[]").split(",");
-        System.out.println("==> Arrays.toString(imageTasksIdsQuery): " + Arrays.toString(imageTasksIdsQuery));
+        String imageTasksIdsValue = getQuery().getValues(REQUEST_ATTR_PROCESSED_IMAGES);
+        if (Objects.nonNull(imageTasksIdsValue)) {
+            String[] imageTasksIdsQuery = imageTasksIdsValue.split(",");
+            System.out.println("==> Arrays.toString(imageTasksIdsQuery): " + Arrays.toString(imageTasksIdsQuery));
+        }
         return new StringRepresentation("TestResource.GET", MediaType.APPLICATION_JSON);
     }
 
