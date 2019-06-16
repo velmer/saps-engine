70286651a4cdc8e2c86068ba00ae2f77cbb6f685
Vélmer Oliveira <oliveira.velmer@gmail.com>
2019-06-16 20:23:38 -0300

    Adds documentation and explanation commentaries

--- scripts/saps_instances_communication_poc.py
+++ scripts/saps_instances_communication_poc.py
@@ -3,26 +3,41 @@
 
 import requests
 
+# HTTP protocol scheme
 HTTP_SCHEME = 'http://'
-LOCALHOST = 'localhost'
+# SAPS instances' server port
 SERVER_PORT = 8091
 
+# IP of SAPS instance 1
 SAPS_INSTANCE_1_IP = '10.11.4.94'
+# IP of SAPS instance 2
 SAPS_INSTANCE_2_IP = '10.11.4.116'
 
+# URL of SAPS instance 1
 SAPS_INSTANCE_1_URL = HTTP_SCHEME + SAPS_INSTANCE_1_IP + ':' + str(SERVER_PORT)
+# URL of SAPS instance 2
 SAPS_INSTANCE_2_URL = HTTP_SCHEME + SAPS_INSTANCE_2_IP + ':' + str(SERVER_PORT)
-LOCAL_URL = HTTP_SCHEME + LOCALHOST + ':' + str(SERVER_PORT)
+
+# URN (API route) of submissions
 PROCESSING_TASKS_URN = '/processings'
 
+# Initial quantity of ImageTasks stored in catalog of instance 1, which must
+# have 3 ImageTasks, one for each satellite (Landsat 5, Landsat 7 e Landsat 8)
 QTY_IMAGE_TASKS_INSTANCE_1 = 3
+# Initial quantity of ImageTasks stored in catalog of instance 2, which must
+# have 0 ImageTasks
 QTY_NO_IMAGE_TASKS = 0
 
+# Key of ID property of ImageTask
 IMAGE_TASK_ID_KEY = 'taskId'
+# Key of State property of ImageTask
 STATE_KEY = 'state'
+
+# Values of ImageTask's State property
 ARCHIVED = 'archived'
 REMOTELY_ARCHIVED = 'remotely_archived'
 
+# Keys of SubmissionParameters
 LOWER_LEFT_KEY = 'lowerLeft[]'
 UPPER_RIGHT_KEY = 'upperRight[]'
 INITIAL_DATE_KEY = 'initialDate'
@@ -31,6 +46,7 @@ INPUT_GATHERING_KEY = 'inputGatheringTag'
 INPUT_PREPROCESSING_KEY = 'inputPreprocessingTag'
 ALGORITHM_EXECUTION_KEY = 'algorithmExecutionTag'
 
+# Values of SubmissionParameters
 LOWER_LEFT_LATITUDE = '-7.913'
 LOWER_LEFT_LONGITUDE = '-37.814'
 UPPER_RIGHT_LATITUDE = '-6.547'
@@ -41,16 +57,25 @@ INPUT_GATHERING = 'Default'
 INPUT_PREPROCESSING = 'Default'
 ALGORITHM_EXECUTION = 'Default'
 
+# Keys of auth credentials
 USER_EMAIL_KEY = 'userEmail'
 USER_PASS_KEY = 'userPass'
 
+# Values of auth credentials
 ADMIN_EMAIL = 'admin@admin.com'
 ADMIN_PASSWORD = '4dm1n'
 
+# Successful feedback of submission processing
 SUBMIT_PROCESSING_SUCCESSFUL = "Tasks successfully added"
 
 
 def get_all_image_tasks(saps_instance_url):
+    """
+    Returns all ImageTasks stored in SAPS instance that had its URL specified.
+
+    :param saps_instance_url: URL of SAPS instance.
+    :return: List of all ImageTasks in SAPS instance catalog.
+    """
     get_all_image_tasks_url = saps_instance_url + PROCESSING_TASKS_URN
     headers = {**get_admin_credentials()}
     response = requests.get(url=get_all_image_tasks_url, headers=headers)
@@ -58,6 +83,13 @@ def get_all_image_tasks(saps_instance_url):
 
 
 def submit_processing(saps_instance_url):
+    """
+    Submits a processing with default SubmissionParameters to the SAPS instance
+    that had its URL specified.
+
+    :param saps_instance_url: URL of SAPS instance.
+    :return: Feedback of submission processing.
+    """
     submit_processing_url = saps_instance_url + PROCESSING_TASKS_URN
     data = {**get_admin_credentials(), **get_default_submission_parameters()}
     response = requests.post(url=submit_processing_url, data=data)
@@ -65,6 +97,9 @@ def submit_processing(saps_instance_url):
 
 
 def get_admin_credentials():
+    """
+    :return: admin credentials for both SAPS instances.
+    """
     return {
         USER_EMAIL_KEY: ADMIN_EMAIL,
         USER_PASS_KEY: ADMIN_PASSWORD
@@ -72,6 +107,9 @@ def get_admin_credentials():
 
 
 def get_default_submission_parameters():
+    """
+    :return: default SubmissionParameters.
+    """
     return {
         LOWER_LEFT_KEY: [LOWER_LEFT_LATITUDE, LOWER_LEFT_LONGITUDE],
         UPPER_RIGHT_KEY: [UPPER_RIGHT_LATITUDE, UPPER_RIGHT_LONGITUDE],
@@ -84,18 +122,22 @@ def get_default_submission_parameters():
 
 
 def main():
+    # Preconditions
     image_tasks_instance_1 = get_all_image_tasks(SAPS_INSTANCE_1_URL)
     image_tasks_instance_2 = get_all_image_tasks(SAPS_INSTANCE_2_URL)
     assert len(image_tasks_instance_1) == QTY_IMAGE_TASKS_INSTANCE_1
     assert len(image_tasks_instance_2) == QTY_NO_IMAGE_TASKS
 
+    # Submits processing to SAPS instance 2
     assert submit_processing(SAPS_INSTANCE_2_URL) == SUBMIT_PROCESSING_SUCCESSFUL
 
+    # Instance 2 must have inserted each ImageTask stored in Instance 1
     image_tasks_instance_1 = get_all_image_tasks(SAPS_INSTANCE_1_URL)
     image_tasks_instance_2 = get_all_image_tasks(SAPS_INSTANCE_2_URL)
     assert len(image_tasks_instance_1) == QTY_IMAGE_TASKS_INSTANCE_1
     assert len(image_tasks_instance_2) == QTY_IMAGE_TASKS_INSTANCE_1
 
+    # Sorts lists of two instances to compare them as pairs
     sorted(image_tasks_instance_1, key=lambda image_task: image_task[IMAGE_TASK_ID_KEY])
     sorted(image_tasks_instance_2, key=lambda image_task: image_task[IMAGE_TASK_ID_KEY])
 
@@ -103,9 +145,12 @@ def main():
         image_task_instance_1 = image_tasks_instance_1[i]
         image_task_instance_2 = image_tasks_instance_2[i]
 
+        # Reused ImageTasks must have the 'Remotely Archived' state
         assert image_task_instance_1[STATE_KEY] == ARCHIVED
         assert image_task_instance_2[STATE_KEY] == REMOTELY_ARCHIVED
 
+        # Except for the State, every other attributes of reused ImageTasks
+        # must be equal to processed ImageTasks, including the ID
         for key in list(image_task_instance_1.keys()):
             if key != STATE_KEY:
                 assert image_task_instance_1[key] == image_task_instance_2[key]
