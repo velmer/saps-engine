77f296558342795f29acb2cf22f4053761a17ba6
Vélmer Oliveira <oliveira.velmer@gmail.com>
2019-06-17 11:20:28 -0300

    Creates toJSON and constructor from JSON for `ImageTaskFile`

--- src/main/java/org/fogbowcloud/saps/engine/core/pojo/ImageTaskFileList.java
+++ src/main/java/org/fogbowcloud/saps/engine/core/pojo/ImageTaskFileList.java
@@ -18,8 +18,6 @@ public class ImageTaskFileList {
     private static final String REGION = "region";
     private static final String COLLECTION_TIER_NAME = "collectionTierName";
     private static final String IMAGE_DATE = "imageDate";
-    private static final String NAME = "name";
-    private static final String URL = "url";
     private static final String FILES = "files";
     private static final String STATUS = "status";
     private static final String UNAVAILABLE = "UNAVAILABLE";
@@ -40,12 +38,7 @@ public class ImageTaskFileList {
         List<ImageTaskFile> imageTaskFiles = new ArrayList<>();
         JSONArray filesJSONArray = jsonObject.getJSONArray(FILES);
         for (int i = 0; i < filesJSONArray.length(); i++) {
-            JSONObject imageTaskFileJSON = filesJSONArray.optJSONObject(i);
-            imageTaskFiles.add(new ImageTaskFile(
-                    null,
-                    imageTaskFileJSON.getString(NAME),
-                    imageTaskFileJSON.getString(URL)
-            ));
+            imageTaskFiles.add(new ImageTaskFile(filesJSONArray.optJSONObject(i)));
         }
         this.imageTask = imageTask;
         this.imageTaskFiles = imageTaskFiles;
@@ -58,14 +51,11 @@ public class ImageTaskFileList {
             jsonObject.put(REGION, imageTask.getRegion());
             jsonObject.put(IMAGE_DATE, imageTask.getImageDate());
             jsonObject.put(COLLECTION_TIER_NAME, imageTask.getCollectionTierName());
-            JSONArray filesJSONArray = new JSONArray();
+            JSONArray imageTaskFilesJSONArray = new JSONArray();
             for (ImageTaskFile imageTaskFile : imageTaskFiles) {
-                JSONObject imageTaskFileJSONObject = new JSONObject();
-                imageTaskFileJSONObject.put(NAME, imageTaskFile.getName());
-                imageTaskFileJSONObject.put(URL, imageTaskFile.getURL());
-                filesJSONArray.put(imageTaskFileJSONObject);
+                imageTaskFilesJSONArray.put(imageTaskFile.toJSON());
             }
-            jsonObject.put(FILES, filesJSONArray);
+            jsonObject.put(FILES, imageTaskFilesJSONArray);
         } catch (JSONException e) {
             jsonObject.put(STATUS, UNAVAILABLE);
             throw e;
